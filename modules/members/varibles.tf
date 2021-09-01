c1d9687937e6cb9029daa9b7012bdf222737f6c5
Ryan den Otter <ryan@ottershome.com>
2021-09-01 12:42:43 -0600

    add member terraform

---
+++ modules/members/varibles.tf
@@ -0,0 +1,8 @@
+variable "developer_list" {
+    description = "List of developers"
+    type        = list
+}
+variable "admin_list" {
+    description = "List of admins"
+    type        = list
+}
